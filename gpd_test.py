8b9a8b1cf8accb040a56ad97055143831e8c5876
vrbaj <vrbik.vrbik@gmail.com>
2020-03-03 18:59:57 +0100

    MLE, closed form, moment estimators

--- gpd_test.py
+++ gpd_test.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 sigma = 1
-gamma = -0.2
+gamma = 0.1
 mu = 0
-sample_size = 10000
+sample_size = 100000
 random_numbers = genpareto.rvs(gamma, loc=mu, scale=sigma, size=sample_size)
 fig, ax = plt.subplots(1, 1)
 ax.hist(random_numbers, density=True, histtype="stepfilled", alpha=0.2, label="rnds")
@@ -20,7 +20,19 @@ sample_mean_sq = np.mean(random_numbers ** 2)
 sample_variance = np.var(random_numbers)
 sample_variance_sq = np.var(random_numbers ** 2)
 sigma_est = 0.5 * sample_mean * sample_mean_sq / (sample_mean_sq - sample_mean ** 2)  # OK
-gamma_est = 0.5 - (sample_mean ** 2 / (2 * (sample_mean_sq - sample_mean ** 2)) ) # ok
-print("sigma est: {}".format(sigma_est))
-print("gamma est: {}".format(gamma_est))
+gamma_est = 0.5 - (sample_mean ** 2 / (2 * (sample_mean_sq - sample_mean ** 2))) # ok
+print("sigma closed est: {}".format(sigma_est))
+print("gamma closed est: {}".format(gamma_est))
+
+sigma_moments = 0.5 * sample_mean * (sample_mean ** 2 / sample_variance + 1)
+gamma_moments = -0.5 * (sample_mean ** 2 / sample_variance - 1)
+print("sigma moments est:  {}".format(sigma_moments))
+print("gamma moments est: {}".format(gamma_moments))
+
+print(sigma_moments - sigma_est)
+print(gamma_moments - gamma_est)
+print(1 - sigma/sigma_moments)
+
+mle_fit = genpareto.fit(random_numbers)
+print(mle_fit)
 plt.show()
